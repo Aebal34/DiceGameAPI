1474e4b645fab836370f9ffaac2ad7ab13c1e68a
Joren <97386268+Aebal34@users.noreply.github.com>
2023-08-24 19:42:47 +0200

    UserDetailsService configured for admin and user

--- src/main/java/com/Utopia/Joren/DiceGameAPI/Security/SecurityConfig.java
+++ src/main/java/com/Utopia/Joren/DiceGameAPI/Security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.Utopia.Joren.DiceGameAPI.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +22,25 @@ public class SecurityConfig {
             http.csrf(AbstractHttpConfigurer::disable);
             http.authorizeHttpRequests(authorize -> authorize
                     .anyRequest().authenticated());
-
+            http.authorizeHttpRequests((authorize -> authorize
+                    .requestMatchers(HttpMethod.GET).permitAll()));
             return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users(){
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
